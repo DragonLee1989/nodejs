285d173d97026651205625c9aaba4544f45343c8
dragonleeaht2019@gmail.com <dragonleeaht2019@gmail.com>
2023-05-29 16:52:21 +0700

    add connect DB

---
+++ src/config/connectDB.js
@@ -0,0 +1,37 @@
+// const { Sequelize } = require('sequelize');
+import Sequelize from 'sequelize';
+
+// // Option 1: Passing a connection URI
+// const sequelize = new Sequelize('sqlite::memory:') // Example for sqlite
+// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
+
+// // Option 2: Passing parameters separately (sqlite)
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: 'path/to/database.sqlite'
+// });
+
+// Option 3: Passing parameters separately (other dialects)
+// let database = "testnodejs";
+// let username = "root";
+// let password = "43214321";
+
+const sequelize = new Sequelize('testnodejs', 'root', '43214321', {
+  host: 'localhost',
+  dialect: 'mysql'
+  /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+});
+
+console.log('Hi connect DB');
+
+let connectDB = async() => {
+  console.log('Hi connect DB');
+    try {
+        await sequelize.authenticate();
+        console.log('Connection DB has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+}
+
+module.exports = connectDB;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
-import viewEngine from "./config/viewEngine"
-import initWebRouters from "./route/web"
+import viewEngine from "./config/viewEngine";
+import initWebRouters from "./route/web";
+import connectDB from "./config/connectDB";
 
 // to run process.env
 require('dotenv').config(); 
@@ -13,9 +14,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 viewEngine(app);
-
 // server vao tat ca route
 initWebRouters(app);
+
+// connect to database mysql
+connectDB;
 
 let port = process.env.PORT;
 
