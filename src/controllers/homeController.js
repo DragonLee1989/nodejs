93cac76cd739a5cc4280a10c63c1fa49911d7b43
dragonleeaht2019@gmail.com <dragonleeaht2019@gmail.com>
2023-06-05 16:20:12 +0700

    add CRUD

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,10 @@
 import db from "../models/index";
+import CRUDService from "../services/CRUDService";
 
 let getHomePage = async (req, res) => {
   try {
     let data = await db.User.findAll();
-    console.log(data);
+    // console.log(data);
 
     return res.render("homepage.ejs", {
       data: JSON.stringify(data),
@@ -17,6 +18,47 @@ let getAboutPage = (req, res) => {
   return res.render("test/about.ejs");
 };
 
+let getCRUD = (req, res) => {
+  return res.render("./crud.ejs");
+};
+
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  // console.log(message);
+  return res.send("POST CRUD");
+};
+
+let findAllCRUD = async (req, res) => {
+  let users = await CRUDService.getAllUser();
+  // console.log(users);
+  return res.render("./showCRUD.ejs", {
+    usersTable: users,
+  });
+};
+
+let getEditCRUD = async (req, res) => {
+  let userId = req.query.id;
+  if (userId) {
+    let userData = await CRUDService.getUserInfoById(userId);
+    console.log(userData);
+    return res.render("./editCRUD.ejs", {
+      user: userData,
+    });
+  } else {
+    return res.send("User not Found!");
+  }
+
+  // console.log(req.query.id);
+};
+
+let putCRUD = async (req, res) => {
+  let data = req.body;
+  let allUsers = await CRUDService.updateUserData(data);
+  return res.render("./showCRUD.ejs", {
+    usersTable: allUsers,
+  });
+};
+
 // object: {
 //  key: '',
 //  value: ''
@@ -24,4 +66,9 @@ let getAboutPage = (req, res) => {
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
+  findAllCRUD: findAllCRUD,
+  getEditCRUD: getEditCRUD,
+  putCRUD: putCRUD,
 };
---
+++ src/services/CRUDService.js
@@ -0,0 +1,113 @@
+import bcrypt from "bcryptjs";
+import db from "../models/index";
+// ham hash password
+const salt = bcrypt.genSaltSync(10);
+
+// createNewUser
+let createNewUser = async (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+      await db.User.create({
+        email: data.email,
+        password: hashPasswordFromBcrypt,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        address: data.address,
+        gender: data.gender == "1" ? true : false,
+        roleid: data.roleid,
+        phoneNumber: data.phoneNumber,
+      });
+      resolve("Created a new User");
+    } catch (error) {
+      reject(error);
+    }
+  });
+
+  // console.log("CLIENT => CONTROLLER => SERVICE: ");
+  // console.log(data);
+  // console.log(hashPasswordFromBcrypt);
+};
+
+// hashUserPassword: ma hoa "password" cua User truoc khi => DB
+let hashUserPassword = (password) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      var hashPassword = await bcrypt.hashSync(password, salt);
+      resolve(hashPassword);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+// getAllUser
+let getAllUser = async () => {
+  // Promise: ham xu ly bat dong bo
+  // resolve <=> return
+  return new Promise(async (resolve, reject) => {
+    try {
+      let users = await db.User.findAll({
+        // raw: only display user array
+        raw: true,
+      });
+      resolve(users);
+    } catch (error) {
+      reject(reject);
+    }
+  });
+};
+
+let getUserInfoById = (userId) => {
+  // Promise: ham xu ly bat dong bo
+  // resolve <=> return
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: userId },
+        raw: true,
+      });
+      if (user) {
+        console.log("User found by Id = " + userId);
+        resolve(user);
+      } else {
+        console.log("User not found by Id = " + userId);
+      }
+    } catch (error) {
+      reject(reject);
+    }
+  });
+};
+
+let updateUserData = (data) => {
+  // Promise: ham xu ly bat dong bo
+  // resolve <=> return
+  return new Promise(async (resolve, reject) => {
+    try {
+      console.log("User can Update Info voi id = " + data.id);
+      let user = await db.User.findOne({
+        where: { id: data.id },
+      });
+      if (user) {
+        user.firstName = data.firstName;
+        user.lastName = data.lastName;
+        user.address = data.address;
+        await user.save();
+
+        let allUsers = await db.User.findAll();
+        resolve(allUsers);
+      } else {
+        resolve();
+      }
+    } catch (error) {
+      reject("Loi updateUserData: " + reject);
+    }
+  });
+};
+
+module.exports = {
+  createNewUser: createNewUser,
+  getAllUser: getAllUser,
+  getUserInfoById: getUserInfoById,
+  updateUserData: updateUserData,
+};
