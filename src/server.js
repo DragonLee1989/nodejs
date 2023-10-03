273eb2c23fa81b972bf02fc54a80dde01bad4193
DragonLee1989 <dragonleeaht2019@gmail.com>
2023-10-03 12:17:15 +0700

    add

--- src/server.js
+++ src/server.js
@@ -3,15 +3,41 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRouters from "./route/web";
 import connectDB from "./config/connectDB";
-import cors from "cors";
+// import cors from "cors";
 
 // to run process.env
 require("dotenv").config();
 
 let app = express();
 
-// "cors": only run api theo domain
-app.use(cors({ origin: true }));
+// "cors": only run api theo domain http://localhost:3000
+// app.use(cors({ origin: true }));
+
+// xu ly loi "cors" Chrome khong cho phep load API khac domain, phai run theo domain http://localhost:3000
+// Add headers before the routes are defined tuong tu "cors"
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", process.env.URL_REACT);
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 // config app
 app.use(bodyParser.json());
